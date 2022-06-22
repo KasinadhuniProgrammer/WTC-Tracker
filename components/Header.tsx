99140ca112ff0412847f5f10567311ce2b3949c5
Satvik Kasinadhuni <rachitipidudraksha@gmail.com>
2022-06-22 13:34:24 +0530

    starting header

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import '../styles/Home.module.css'
+
+const Header: NextPage = () => {
+    return (
+        <div className="header">
+            <h3 className="header__title">World Test Championship Tracker</h3>
+        </div>
+    )
+}
+
+export default Header
