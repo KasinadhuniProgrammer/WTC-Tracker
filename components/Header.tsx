54a2a094c826e796a0a6bef82233f49cc9045082
Satvik Kasinadhuni <rachitipidudraksha@gmail.com>
2022-06-23 09:50:10 +0530

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,21 @@ import '../styles/Home.module.css'
 const Header: NextPage = () => {
     return (
         <div className="header">
-            <h3 className="header__title">World Test Championship Tracker</h3>
+            <div className="header__title">
+                <h3><a href="https://www.icc-cricket.com/world-test-championship" target="_blank">WTC</a></h3>
+            </div>
+            <div className="header__anchor1">
+                <a>Results</a>
+            </div>
+            <div className="header__anchor2">
+                <a>Fixtures</a>
+            </div>
+            <div className="header__anchor3">
+                <a>Stats</a>
+            </div>
+            <div className="header__anchor4">
+                <a>Standings</a>
+            </div>
         </div>
     )
 }
